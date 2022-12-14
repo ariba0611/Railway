54fb097d70a1c5a93fcecedbab544ed5c920a291
Ariba Siddiqui <aribasiddiqui1111@gmail.com>
2022-12-14 15:36:24 +0000

    Finally

--- Railway/Controllers/HomeController.cs
+++ Railway/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Railwa.Models;
 using Railway.Models;
 using Railway.Models.DAL;
 
@@ -19,27 +20,51 @@ namespace Railway.Controllers
             this.interfaceObj = new Repository<Reservation>();
 
         }
-        [HttpGet]
+      
         public ActionResult Index()
         {
             var data = interfaceObj.GetModel();
             return View(data);
         }
-        
+        public ActionResult Create()
+        {
+            return View();  
+        }
 
         [HttpPost]
-        public ActionResult Index(Reservation res)
+        public ActionResult Create(Reservation res)
 
         {
-            if (ModelState.IsValid)
+
+            var data = interfaceObj.GetModel();
+            if (data.Count() >= 7)
             {
+                TempData["msg"] = "<script>alert('YOU CANNOT ADD MORE THAN 6 PASSENGER');</script>";
+            }
+            else
+            {
+
                 interfaceObj.InsertModel(res);
                 interfaceObj.Save();
+
                 return RedirectToAction("Index");
             }
-
             return View(res);
            
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Reservation rt = interfaceObj.GetModelByID(id);
+            return View(rt);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Reservation reservation)
+        {
+
+            interfaceObj.DeleteModel(id);
+            interfaceObj.Save();
+            return RedirectToAction("Index");
         }
 
 
